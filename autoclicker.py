e3c2ef9f29930065c4456a2049a9b5096b5b4df8
agentN86 <agentn86@duck.com>
2024-12-27 15:33:25 -0600

    new update

--- autoclicker.py
+++ autoclicker.py
@@ -3,6 +3,7 @@ import threading
 import time
 from pynput.mouse import Button, Controller
 import keyboard
+import webbrowser
 
 class AutoClicker:
     def __init__(self, master):
@@ -16,24 +17,30 @@ class AutoClicker:
         self.is_running = False
         self.mouse = Controller()
         self.keybind = 'F6'
-        
-        self.labela = tk.Label(master, text="minutes")
+
+        self.labela = tk.Label(master, text="hours")
         self.labela.place(x=60, y=3)
+        self.hoursEntry = tk.Entry(master, width=8)
+        self.hoursEntry.insert(0, "0")
+        self.hoursEntry.place(x=5, y=3)
+        
+        self.labelb = tk.Label(master, text="minutes")
+        self.labelb.place(x=60, y=25)
         self.minutesEntry = tk.Entry(master, width=8)
         self.minutesEntry.insert(0, "0")
-        self.minutesEntry.place(x=5, y=3)
+        self.minutesEntry.place(x=5, y=25)
 
-        self.labelb = tk.Label(master, text="secs")
-        self.labelb.place(x=60, y=25)
+        self.labelc = tk.Label(master, text="secs")
+        self.labelc.place(x=60, y=47)
         self.secondsEntry = tk.Entry(master, width=8)
         self.secondsEntry.insert(0, "0")
-        self.secondsEntry.place(x=5, y=25)
+        self.secondsEntry.place(x=5, y=47)
 
-        self.labelc = tk.Label(master, text="milliseconds")
-        self.labelc.place(x=60, y=47)
+        self.labeld = tk.Label(master, text="milliseconds")
+        self.labeld.place(x=60, y=69)
         self.millisecondsEntry = tk.Entry(master, width=8)
         self.millisecondsEntry.insert(0, "0")
-        self.millisecondsEntry.place(x=5, y=47)
+        self.millisecondsEntry.place(x=5, y=69)
 
         self.typeofclick = tk.IntVar(value=1)
         tk.Radiobutton(root, text='Left Click', variable=self.typeofclick, value=1).place(x=150, y=3)
@@ -41,6 +48,9 @@ class AutoClicker:
         
         self.keybind_button = tk.Button(master, text="Keybind Settings", command=self.openkeybindwin, width="18", pady="5")
         self.keybind_button.place(x=12, y=143)
+
+        self.helpabout_button = tk.Button(master, text="About", command=self.openhelpwin, width="18", pady="5")
+        self.helpabout_button.place(x=150, y=143)
 
         self.start_button = tk.Button(master, text="Start (F6)", command=self.start_clicking, width="18", pady="5")
         self.start_button.place(x=12, y=180)
@@ -58,11 +68,12 @@ class AutoClicker:
             return
 
         try:
+            hours = float(self.hoursEntry.get())
             minutes = float(self.minutesEntry.get())
             seconds = float(self.secondsEntry.get())
             milliseconds = float(self.millisecondsEntry.get())
 
-            equation = (minutes * 60) + seconds + (milliseconds / 1000)
+            equation = (hours * 3600) + (minutes * 60) + seconds + (milliseconds / 1000)
 
             self.interval = equation
             if self.interval < 0.001:
@@ -114,18 +125,36 @@ class AutoClicker:
         def apply_keybind():
             new_keybind = entry.get().upper()
             if new_keybind:
-                keyboard.remove_hotkey(self.keybind)
-                self.keybind = new_keybind
-                keyboard.add_hotkey(self.keybind, self.toggle)
-                self.status_label.config(text=f"Keybind updated to {self.keybind}", fg="black")
-
-                self.start_button.config(text="Start ({})".format(self.keybind))
-                self.stop_button.config(text="Stop ({})".format(self.keybind))
-
-                newwin.destroy()
+                try:
+                    keyboard.remove_hotkey(self.keybind)
+                    keyboard.add_hotkey(new_keybind, self.toggle)
+                    self.keybind = new_keybind
+                    self.status_label.config(text=f"Keybind updated to {self.keybind}", fg="black")
+                    self.start_button.config(text="Start ({})".format(self.keybind))
+                    self.stop_button.config(text="Stop ({})".format(self.keybind))
+                    newwin.destroy()
+                except Exception as e:
+                    self.status_label.config(text=f"Error setting keybind, please reopen the app.", fg="red")
+                    newwin.destroy()
 
         applyButton = tk.Button(newwin, text="Apply", width="5", pady="0", command=apply_keybind)
         applyButton.place(x=90, y=3)
+
+    def openhelpwin(self):
+        newwin = tk.Toplevel(self.master)
+        newwin.title("About")
+        newwin.geometry("230x100")
+        newwin.resizable(False, False)
+
+        def callback(url):
+            webbrowser.open_new(url)
+
+        labela = tk.Label(newwin, text="Auto Clicker v1.0.0", font=("Arial", 13))
+        labela.pack()
+        labelb = tk.Label(newwin, text="GitHub (agentn86)", cursor="hand2", fg="blue")
+        labelb.pack()
+        labelb.bind("<Button-1>", lambda e: callback('https://github.com/agentN86/autoclicker'))
+        
 
 
 if __name__ == "__main__":
